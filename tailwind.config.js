c22f095e4df483dd5fc07d35491a10b5fd2f2f02
abderrahmane <abderrahamane7@gmail.com>
2022-04-20 10:41:11 +0000

    added daysiui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,14 @@ module.exports = {
         extend: {},
     },
     plugins: [require("daisyui")],
+    daisyui: {
+        styled: true,
+        themes: true,
+        base: true,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: "",
+        darkTheme: "dark",
+    },
 };
