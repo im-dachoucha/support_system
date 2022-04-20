70806afcf34fec11d54f2724c07cc2c1ff8dd355
abderrahmane <abderrahamane7@gmail.com>
2022-04-20 10:49:20 +0000

    added authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth'])->name('home');
+
+
+// * auth routes
+Route::get('register', [RegisterController::class, 'index'])->name('auth.register');
+Route::post('register', [RegisterController::class, 'store']);
+
+Route::get('login', [LoginController::class, 'index'])->name('auth.login');
+Route::post('login', [LoginController::class, 'store']);
+
+Route::post('logout', [LogoutController::class, 'index'])->name('auth.logout');
