85e598ee626163902dee40a269ba038bbcd659fe
abderrahmane <abderrahamane7@gmail.com>
2022-04-20 10:54:07 +0000

    added index method to ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 
 class TicketController extends Controller
 {
-    //
+    public function index()
+    {
+        if(Blade::check('admin')){
+            return view('tickets.index', ["tickets" => Ticket::get(1)]);
+        }
+        // return view('tickets.index', ["tickets" => Ticket::where('user_id', auth()->user()->id)->paginate(1)]);
+        return view('tickets.index', ["tickets" => auth()->user()->tickets]);
+    }
 }
