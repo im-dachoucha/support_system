45b4fdbb17f9a2473ab362ca8f232432cb76b6fa
abderrahmane <abderrahamane7@gmail.com>
2022-04-21 22:11:09 +0000

    added admin and user middleware

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user() && !auth()->user()->is_admin){
+            return $next($request);
+        }
+        return back()->withErrors([
+            'message' => 'You are not authorized to access this page.'
+        ]);
+    }
+}
