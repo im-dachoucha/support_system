95d2b49ea914c12a1ce4123b07a419de5e7d9dd2
abderrahmane <abderrahamane7@gmail.com>
2022-04-21 22:14:36 +0000

    removed auth middleware from home route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware(['auth'])->name('home');
+})->name('home');
 
 
 // * auth routes
