583ffc32fa1c09bc648175c2a46f756ad1588e17
abderrahmane <abderrahamane7@gmail.com>
2022-04-22 11:20:17 +0000

    index and store methods for services

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('admin');
+    }
+
+    public function index()
+    {
+        return view('services.index', ['services' => Service::paginate(5)]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'entitled' => 'required|string|min:5|max:20|unique:services,entitled',
+        ]);
+
+        Service::create([
+            'entitled' => $request->entitled
+        ]);
+
+        return back();
+    }
 }
